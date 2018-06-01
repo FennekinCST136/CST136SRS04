489d25c3da683fc35e67bb0c7a76d2102f3f5756
Fennekin <colin.rexing@gmail.com>
2018-06-01 16:17:08 -0700

    updated range class

--- Atoll/range.h
+++ Atoll/range.h
@@ -1,4 +1,7 @@
 ﻿#pragma once
+#include <limits>
+#include <string>
+
 
 template<typename T, T min, T max>
 class Range // You should re-code and simplify your SRS01 RangeInt
@@ -6,8 +9,21 @@ class Range // You should re-code and simplify your SRS01 RangeInt
 private:
 	using value_type = T;
 
-	value_type value_;
+	value_type lower_bound_{ std::numeric_limits<int>::min() };
+	value_type upper_bound_{ std::numeric_limits<int>::max() - 1 };
+	value_type value_{ std::numeric_limits<int>::max() };
+
+
 
 public:
-	Range(const value_type value);
+	Range(value_type const value)
+	{
+		static_assert (value >= lower_bound_ && value < upper_bound_);
+		value_ = value;
+	}
+
+	value_type get_value() const noexcept
+	{
+		return value_;
+	}
 };
